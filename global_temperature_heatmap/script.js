c1ff57a295cf9d136ee2c5dc66ecf7e5e698a305
Anh Nguyen <nguyenmaianh.96@gmail.com>
2022-11-03 19:35:24 +0700

    edit

---
+++ global_temperature_heatmap/script.js
@@ -0,0 +1,40 @@
+//Link to dataset
+var url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json";
+
+//Width and height
+var w = 900;
+var h = 460;
+var padding = 20;
+
+//Create SVG element
+
+
+//For converting strings to date
+
+
+//Create tooltip
+
+
+//Create color
+
+
+//Load data and create chart
+d3.json(url)
+    .then(data => {
+        console.log(data);
+    })
+
+//Convert string to time
+
+
+//Create scale functions
+
+
+//Create axes
+
+
+//Create bar/circle/dot/etc
+
+
+//Add legend
+
